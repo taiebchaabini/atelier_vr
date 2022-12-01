2502007979ea55ca436a382795364ddf8e8935a0
Redginald Godeau <>
2022-12-01 03:53:35 +0100

    Adding some sound

--- Assets/Atelier VR/Scripts/DoorOpening.cs
+++ Assets/Atelier VR/Scripts/DoorOpening.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class DoorOpening : MonoBehaviour
 {
+    public AudioClip[] actions;
+    public AudioSource audioComp;
     private void OnTriggerEnter(Collider other) {
         GetComponent<Animator>().SetBool("character_nearby", true);
+        audioComp.Stop();
+        audioComp.clip = actions[1];
+        audioComp.Play();
     }
 
     private void OnTriggerExit(Collider other) {
         GetComponent<Animator>().SetBool("character_nearby", false);
+        audioComp.Stop();
+        audioComp.clip = actions[0];
+        audioComp.Play();
     }
 }
